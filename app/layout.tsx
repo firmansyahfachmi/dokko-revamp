d8639898ca80350913a81c14a0793bcd6d3d42e3
firmansyahfachmi <firmansyahfachmi5@gmail.com>
2024-05-22 17:30:10 +0700

    adding og tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,27 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="icon" href="/icons/loominace-favico.jpg" sizes="any" />
+
         <meta property="og:title" content="loominace" />
-        <meta property="og:url" content="https://loominace.com" />
-        <meta property="og:type" content="image/jpeg" />
+        <meta property="og:url" content="https://www.loominace.com" />
+        <meta property="og:type" content="website" />
         <meta property="og:image" content="/images/logo-og.jpg" />
         <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="600" />
+        <meta property="og:image:height" content="630" />
         <meta
           property="og:description"
           content="Discover unrivaled excellence as we elevate your brand, exceeding expectations at every turn."
         />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="loominac.com" />
+        <meta property="twitter:url" content="https://www.loominace.com" />
+        <meta name="twitter:title" content="loominace" />
+        <meta
+          name="twitter:description"
+          content="Discover unrivaled excellence as we elevate your brand, exceeding expectations at every turn."
+        />
+        <meta name="twitter:image" content="/images/logo-og.jpg" />
       </head>
       <body>{children}</body>
     </html>
